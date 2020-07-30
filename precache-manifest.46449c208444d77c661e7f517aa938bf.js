bc7bad43d5a2a7814a238e3babacbe7ae906a692
Ailun Shen <newhoopsdynasty@gmail.com>
2020-07-29 22:25:30 -0400

    Updates

---
+++ precache-manifest.46449c208444d77c661e7f517aa938bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b9eba7f3df51236bf2c90e2e9cb546",
+    "url": "/pwa-demo/index.html"
+  },
+  {
+    "revision": "0a8439446094792dd102",
+    "url": "/pwa-demo/static/css/main.e385804d.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/pwa-demo/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-demo/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8439446094792dd102",
+    "url": "/pwa-demo/static/js/main.cfb7cc4f.chunk.js"
+  },
+  {
+    "revision": "62d37408b512e7b4f653",
+    "url": "/pwa-demo/static/js/runtime-main.80acdee8.js"
+  },
+  {
+    "revision": "80e19d92838adbc8905385af48999139",
+    "url": "/pwa-demo/static/media/bg.80e19d92.jpg"
+  }
+]);
